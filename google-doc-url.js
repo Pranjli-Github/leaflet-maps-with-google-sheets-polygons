a34aacc650ef6d6f784b7329e6858373ad0ec62a
Pranjli-Github <57127798+Pranjli-Github@users.noreply.github.com>
2019-11-17 14:32:05 +0530

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/18rnX-yoX6Q-1TfBghFRrUi2bKm6NJZN_DikAa1KU4UE/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/17kF1FnxM00n7k0eK0x6F0YTKjZVK2B6dnfvGxD4b8N8/edit#gid=0';
